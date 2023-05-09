aa687750e61556f5e84002bec11f6099c6af5361
Kumar Ameesha Ramesh <kumar.ameesha1@gmail.com>
2023-05-09 15:14:10 +0900

    done with challenge 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # create a new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create', as: :tasks
+
+  # List all the tasks -> index
+  get '/tasks', to: 'tasks#index'
+
+  # get the description
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Destroy
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
